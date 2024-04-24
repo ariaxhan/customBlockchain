b23115459bfb152e1f68d5336b25b11751b97203
Aria <113392746+ariaxhan@users.noreply.github.com>
2024-04-23 20:47:43 -0700

    trying to do hashing

--- blockchain.py
+++ blockchain.py
@@ -46,12 +46,17 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
-    @staticmethod
-    def hash(block):
-        # Hashes a Block
-        pass
-
     @property
     def last_block(self):
-        # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
